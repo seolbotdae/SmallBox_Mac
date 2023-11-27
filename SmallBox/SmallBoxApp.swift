d2032db5518ef1b9a0f2b23020659324a510561a
seolbotdae <seolbotdae@gmail.com>
2023-11-27 16:25:28 +0900

    Initial Commit

---
+++ SmallBox/SmallBoxApp.swift
@@ -0,0 +1,32 @@
+//
+//  SmallBoxApp.swift
+//  SmallBox
+//
+//  Created by 설우혁 on 11/27/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SmallBoxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
